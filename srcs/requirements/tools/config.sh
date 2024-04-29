f18b88db3ca2ef88a1da8ee7a00395d1830884bb
Felix von Nagel <fvon-nag@student.42wolfsburg.de>
2024-04-29 13:53:59 +0200

    fixed bugs updated files

--- srcs/requirements/tools/config.sh
+++ srcs/requirements/tools/config.sh
@@ -2,18 +2,11 @@
 echo "=== INCEPTION CONFIGURATION ==="
 
 printf "\nEnter the path where to create your mariadb database and wordpress files"
-printf "\nExahomple : /home/fvon-nag/data\n"
+printf "\nExample : /home/fvon-nag/data\n"
 read path
 
-#if this repertory doesn't exist, create it and set the permissions
-if [ ! -d "$path" ]; then
-    echo "This repertory doesn't exist, please create it"
-    exit 1
-fi
 
 echo "$path" > srcs/requirements/tools/data_path.txt
-
-cp srcs/requirements/tools/template_compose.yml srcs/docker-compose.yml
 
 # replace 'path/to/data' by the path entered by the user in srcs/docker-compose.yml
 cat srcs/docker-compose.yml | sed "s+pathtodata+$path+g" > srcs/docker-compose.yml.tmp
