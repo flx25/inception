aaed577bb90a39c40968e3bf851312cdb7f6d7e7
Felix von Nagel <fvon-nag@student.42wolfsburg.de>
2024-05-03 11:52:48 +0200

    getting to wp landing page now, installation still does not work

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,26 +1,26 @@
-#!bin/bash
+#!/bin/bash
 sleep 10
-if [ ! -e /var/www/wordpress/wp-config.php ]; then
-    wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
-    					--dbhost=mariadb:3306 --path='/var/www/wordpress' 
-                    
+
+# Create wp-config.php file manually
+cat << EOF > /var/www/wordpress/wp-config.php
+<?php
+define('DB_NAME', '${SQL_DATABASE}');
+define('DB_USER', '${SQL_USER}');
+define('DB_PASSWORD', '${SQL_PASSWORD}');
+define('DB_HOST', 'mariadb:3306');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
+\$table_prefix = 'wp_';
+/* That's all, stop editing! Happy publishing. */
+require_once(ABSPATH . 'wp-settings.php');
+EOF
 
 sleep 2
-wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
-fi
-
-# echo "define( 'CONCATENATE_SCRIPTS', false );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'SCRIPT_DEBUG', true );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_HOME', 'https://jcluzet.42.fr' );" >> /var/www/wordpress/wp-config.php
-# echo "define( 'WP_SITEURL', 'https://jcluzet.42.fr' );" >> /var/www/wordpress/wp-config.php
-
- echo "define( 'WP_DEBUG', true);" >> /var/www/wordpress/wp-config.php
- echo "define( 'WP_DEBUG_LOG', true);" >> /var/www/wordpress/wp-config.php
- echo "define( 'WP_DEBUG_DISPLAY', false);" >> /var/www/wordpress/wp-config.php
-# echo "define('WP_ALLOW_REPAIR', true);" >> /var/www/wordpress/wp-config.php
-
-	
+wp core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
+wp user create --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
 
 # if /run/php folder does not exist, create it
 if [ ! -d /run/php ]; then
